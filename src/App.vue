2b68b2bc42b3740e9fbc36830065dae27813385f
manuelcamaioni <camaioni.manuel@gmail.com>
2023-09-04 16:26:59 +0200

    imported Header.vue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
-import { RouterView } from 'vue-router';
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header
+  }
+}
 
 
 </script>
 
 <template>
-  <RouterView>
-  </RouterView>
+  <Header />
+  <router-view>
+  </router-view>
 </template>
 
 <style lang="scss">
